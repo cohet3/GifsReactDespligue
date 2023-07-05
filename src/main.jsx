d90c2bd056d765da77ff5620f90e28c88d734f9c
cohet3 <tauro.total90@gmail.com>
2023-07-05 22:32:23 +0200

    commit de pruebas

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { GifExpertApp } from "./GifExpertApp";
 import "./styles.css";
 import Pruebacss from "./components/Pruebacss";
+import Camara from "./components/Camara";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GifExpertApp />
+    {/* <GifExpertApp /> */}
     {/* <Pruebacss /> */}
     {/* <PruebaBou /> */}
+    <Camara />
   </React.StrictMode>
 );
